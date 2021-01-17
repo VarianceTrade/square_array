91640c408722e2ed81392f37fd3fa1a173fbf3ff
Brandon <sugb73@gmail.com>
2021-01-17 17:15:17 -0500

    commit tree 3587aa5d0f0c442b3ad5de4b9f94b302948bd13d

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,26 @@
 def square_array(array)
-  # your code here
+  
+  # puts numbers
+  new_array = []
+  array.each do |number| # [1,2,3]
+    # number = 1, number = 2, number = 3
+    # Square the number 
+    # Push the number into the new array
+    new_array.push(number * number)
+  end
+  return new_array # [1,4,9]
 end
+
+
+# def calls_on_each(array)
+#   numbers = [1, 2, 3]
+#   numbers.each do |number|
+#     puts number
+#   end
+# end
+
+
+
+
+
+
